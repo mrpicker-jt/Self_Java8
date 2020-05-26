0abf3ae378983f184e6b73052b2af87e55652b4b
TengJiang <1937419351@qq.com>
2020-05-26 18:30:15 +0800

    Pet Creator

--- com/mrpicker/Test.java
+++ com/mrpicker/Test.java
@@ -1,29 +1,13 @@
 package com.mrpicker;
 
-import java.util.stream.IntStream;
+import com.mrpicker.pets.ForNameCreator;
+import com.mrpicker.pets.PetCounter;
+
+import java.util.stream.Stream;
 
 public class Test {
-    static class Cookie {
-     static long counter;
-     final long id=counter++;
-
-
-        @Override
-        public String toString() {
-            return String.valueOf(id);
-        }
-    }
 
     public static void main(String[] args) {
-        Class<Cookie> cookieClass = Cookie.class;
-        IntStream.range(0,5).forEach(i -> {
-            try {
-                System.out.println(cookieClass.newInstance());
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        });
+        PetCounter.count();
     }
 }
