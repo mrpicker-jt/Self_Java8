208b678039eceb0c69489369e1eeeb4594049f46
TengJiang <tengjiang@6tiantian.com>
2020-05-26 23:32:01 +0800

    proxy

--- com/mrpicker/Test.java
+++ com/mrpicker/Test.java
@@ -2,12 +2,14 @@ package com.mrpicker;
 
 import com.mrpicker.pets.ForNameCreator;
 import com.mrpicker.pets.PetCounter;
+import com.mrpicker.pets.ShowMethod;
 
 import java.util.stream.Stream;
 
 public class Test {
 
     public static void main(String[] args) {
-        PetCounter.count();
+//        PetCounter.count();
+        ShowMethod.show("com.mrpicker.Test");
     }
 }
